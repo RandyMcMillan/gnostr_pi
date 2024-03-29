3f6d438a094779dab1c16313845dfc276ed52a47
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-29 10:52:34 -0400

    src/main.rs:count arg

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use num_bigint::BigInt;
-
+use std::{env,process,str::FromStr};
 fn main() {
-    calc_pi();
+
+for argument in env::args() {
+    println!("{}", argument);
+
+let count = match u64::from_str(&argument) {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Error: Could not parse string to u64");
+            return; // Exit the function if parsing fails
+        }
+    };
+
+
+    calc_pi(count as u64);
 }
 
-fn calc_pi() {
+    //calc_pi(argument as u64);
+}
+
+fn calc_pi(count: u64) {
     let mut q = BigInt::from(1);
     let mut r = BigInt::from(0);
     let mut t = BigInt::from(1);
@@ -12,9 +28,12 @@ fn calc_pi() {
     let mut n = BigInt::from(3);
     let mut l = BigInt::from(3);
     let mut first = true;
+    let mut count = 0u32;
     loop {
+        if count >= 10 {process::exit(0);}
         if &q * 4 + &r - &t < &n * &t {
-            print!("{}", n);
+            print!("count={}\n", count);
+            print!("{}\n", n);
             if first {
                 print!(".");
                 first = false;
@@ -33,5 +52,6 @@ fn calc_pi() {
             n = nn;
             r = nr;
         }
+    count = count + 1u32;
     }
 }
