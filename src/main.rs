d50ea00e1c9fc3c0c98e121062e5d2ea804b99d8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-29 11:50:11 -0400

    src/main.rs:calc_pi_with_offset

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,53 @@ fn main() {
     if (args.len() - 1) == 2 {
         let offset = u64::from_str(&args[2]).unwrap();
         println!("offset={}", offset);
+        calc_pi_with_offset(limit as u64,offset as u64);
+        process::exit(0);
     }
 
     calc_pi(limit as u64);
+}
+fn calc_pi_with_offset(limit: u64,offset:u64) {
+    //println!("limit={}", limit);
+    let mut q = BigInt::from(1);
+    let mut r = BigInt::from(0);
+    let mut t = BigInt::from(1);
+    let mut k = BigInt::from(1);
+    let mut n = BigInt::from(3);
+    let mut l = BigInt::from(3);
+    let mut first = true;
+    let mut count = 0u64;
+    //println!("count={}", count);
+    loop {
+        if count == limit {
+            //println!("limit={}", limit);
+            //println!("count={}", count);
+            process::exit(0);
+        }
+        if &q * 4 + &r - &t < &n * &t {
+            //print!("count={}\n", count);
+            if first {
+                //print!("3.");
+                first = false;
+            } else {
+                print!("{}", n);
+            }
+            let nr = (&r - &n * &t) * 10;
+            n = (&q * 3 + &r) * 10 / &t - &n * 10;
+            q *= 10;
+            r = nr;
+        } else {
+            let nr = (&q * 2 + &r) * &l;
+            let nn = (&q * &k * 7 + 2 + &r * &l) / (&t * &l);
+            q *= &k;
+            t *= &l;
+            l += 2;
+            k += 1;
+            n = nn;
+            r = nr;
+        }
+        count = count + 1u64;
+    }
 }
 
 fn calc_pi(limit: u64) {
