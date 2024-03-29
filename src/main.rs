1e3ec77a048d483a99991a57fb9b0b70d1543f30
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-28 23:47:56 -0400

    src/main.rs:new

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,53 @@
 use std::env;
 
+fn g(q: u64, r: u64, t: u64, k: u64, n: u64, l: u64) -> f64 {
+    if 4 * q + r - t < n * t {
+        n as f64 / 10f64.powi(l)
+    } else {
+        g(
+            10 * q,
+            10 * (r - n * t),
+            t,
+            k,
+            (10 * (3 * q + r)) / t - 10 * n,
+            l,
+        ) + g(
+            q * k,
+            (2 * q + r) * l,
+            t * l,
+            k + 1,
+            (q * (7 * k + 2) + r * l) / (t * l),
+            l + 2,
+        )
+    }
+}
+
 fn spigot(n_digits: u32) -> f64 {
-    let mut q = 1.0;
-    let mut r = 0.0;
-    let mut t = 1.0;
-    let mut k = 1.0;
-    let mut n = 0.0;
     let mut sum = 0.0;
+    let (mut q, mut r, mut t, mut k, mut n, mut l) = (1, 0, 1, 1, 3, 3);
 
-    loop {
-        let digit = (k as f64 * q + r) / t;
-        println!("digit={}", digit);
-        sum += digit / 10.0f64.powi(n as i32);
-        println!("sum={}", sum);
-        n += 1.0;
-
-        if n >= n_digits as f64 {
-            return sum;
-        }
-
-        let new_t = 3.0 * q + 2.0 * r;
-        let new_r = 10.0 * r - 5.0 * k * q;
+    while n < n_digits {
+        sum += g(q, r, t, k, n, l);
+        let new_t = 3 * q + 2 * r;
+        let new_r = 10 * r - 5 * k * q;
         q = t;
         r = new_r;
         t = new_t;
         k = -k;
+        n += 1;
     }
+
+    sum
 }
 
 fn main() {
-    let n_digits = 100.0; // Adjust this value for desired precision
-    let pi = spigot(n_digits as u32);
-    println!("Pi ({} digits): {}", n_digits as i32, pi);
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        panic!("Usage: pi_spigot <n_digits>");
+    }
+
+    let n_digits = args[1].parse::<u32>().expect("Invalid number of digits");
+    let pi = spigot(n_digits);
+    println!("Pi ({} digits): {}", n_digits, pi);
 }
 
-//fn main() {
-//    let args = env::args().collect::<Vec<String>>();
-//    if args.len() < 2 {
-//        panic!("Usage: pi_spigot <n_digits>");
-//    }
-//
-//    let n_digits = args[1].parse::<u32>().expect("Invalid number of digits");
-//    let pi = spigot(n_digits);
-//    println!("Pi ({} digits): {}", n_digits, pi);
-//}
-//
